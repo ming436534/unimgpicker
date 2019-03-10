80495b7498ea9f244b8072d5d97066d5e5e39a29
Terence Lee <terencelee1234@gmail.com>
2019-03-11 02:24:34 +0800

    fixed multidex issue!!!!!

--- Assets/Unimgpicker/Editor/XcodeConfigurator.cs
+++ Assets/Unimgpicker/Editor/XcodeConfigurator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_IOS
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -42,3 +43,4 @@ namespace Kakera.Unimgpicker
         }
     }
 }
+#endif
